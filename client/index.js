5c4586240711f6bd4ff3d37f64c8958dd203f4c6
Joanna Kao <joanna.kao@ft.com>
2017-03-04 15:04:11 -0500

    set up react

--- client/index.js
+++ client/index.js
@@ -1,19 +1,14 @@
-/*
-  TODO: delete this comment
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-  This file is where you bootstrap your JS code
-  For example import stuff here:
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is a test</p>
+      </div>
+    );
+  }
+}
 
-  import {select} from 'd3-selection';
-  import myComponent from './components/my-component';
-
-  Split logical parts of you project into components e.g.
-
-  /client
-    - /components
-        - /component-name
-            - styles.scss
-            - index.js
-            - template.html
-
-*/
+ReactDOM.render(<App />, document.getElementById('userinput-wrapper'));
