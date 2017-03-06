e2ecfec6320bb39847aae760dd5d987b684b0fd9
Joanna Kao <joanna.kao@ft.com>
2017-03-06 18:41:21 -0500

    set up reactfauxdom for generating svg with d3

--- client/components/chart/index.js
+++ client/components/chart/index.js
@@ -5,11 +5,43 @@ import * as d3 from 'd3';
 class ColumnChart extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      // Placeholder content displayed before chart render
+      chart: 'Loading chart…',
+    };
+
+    for (const mixin in ReactFauxDOM.mixins.anim) { // eslint-disable-line
+      if ({}.hasOwnProperty.call(ReactFauxDOM.mixins.anim, mixin)) {
+        this[mixin] = ReactFauxDOM.mixins.anim[mixin].bind(this);
+      }
+    }
+
+    for (const mixin in ReactFauxDOM.mixins.core) { // eslint-disable-line
+      if ({}.hasOwnProperty.call(ReactFauxDOM.mixins.core, mixin)) {
+        this[mixin] = ReactFauxDOM.mixins.core[mixin].bind(this);
+      }
+    }
+  }
+
+  componentDidMount() {
+    const faux = this.connectFauxDOM('div.renderedD3', 'chart');
+
+    d3.select(faux)
+      .append('div')
+      .html('Hello World!');
+
+    this.animateFauxDOM(800);
   }
 
   render() {
     return (
-      <img src="images/demo-chart.png" alt="chart" />
+      <div>
+        <div className="renderedD3">
+          {this.state.chart}
+        </div>
+        <img src="images/demo-chart.png" alt="chart" />
+      </div>
     );
   }
 }
