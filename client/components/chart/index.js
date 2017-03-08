3fa1cd074c27a5d670912faa963f5ecaf6e240cd
Joanna Kao <joannaskao@gmail.com>
2017-03-08 02:52:22 -0500

    create column chart using formatted dummy data (temporary)

--- client/components/chart/index.js
+++ client/components/chart/index.js
@@ -10,7 +10,7 @@ class ColumnChart extends Component {
       // Placeholder content displayed before chart render
       chart: 'Loading chart…',
       width: 600,
-      height: 500,
+      height: 300,
     };
 
     for (const mixin in ReactFauxDOM.mixins.anim) { // eslint-disable-line
@@ -27,47 +27,115 @@ class ColumnChart extends Component {
   }
 
   componentDidMount() {
-    console.log(this.props.data);
-    const data = this.props.data.map((d) => {
-      const value = (d['Current stock price'] * d['shares outstanding']) + d['Net Debt'] + d['Minority Interest'];
-      return {
-        category: d.key,
-        value,
-      };
-    });
+    // console.log(this.props.data);
+    // const data = this.props.data.map((d) => {
+    //   const value = (d['Current stock price'] * d['shares outstanding']) + d['Net Debt'] + d['Minority Interest'];
+    //   return {
+    //     category: d.key,
+    //     value,
+    //   };
+    // });
 
-    console.log(data);
+    // formatted dummy data to test d3 column chart generating code
+    const data = [
+      {
+        category: 'Campbell',
+        value: 24,
+      },
+      {
+        category: 'Colgate',
+        value: 50,
+      },
+      {
+        category: 'Clorox',
+        value: 100,
+      },
+      {
+        category: 'Kellogg\'s',
+        value: 30,
+      },
+      {
+        category: 'Mondelez',
+        value: 10,
+      },
+      {
+        category: 'Unilever',
+        value: 153,
+      },
+    ];
 
     const chart = this.connectFauxDOM('svg', 'chart');
     const margin = { // Mike Bostock's margin convention
       top: 20,
-      right: 28,
+      right: 40,
       bottom: 30,
       left: 0,
     };
     const width = this.state.width - margin.left - margin.right;
     const height = ((this.state.height - margin.top) - margin.bottom) + 14;
-    const yDomainMax = (5 * Math.ceil(d3.max(data) / 5)) + 5;
-    const x1 = d3.scaleLinear()
-        .domain([0, 100])
-        .range([0, width]);
-    const y = d3.scaleLinear()
+    const yDomainMax = (5 * Math.ceil(d3.max(data.map(d => d.value)) / 5)) + 5;
+
+    const xScale = d3.scaleBand()
+        .domain(data.map(d => d.category))
+        .range([0, width])
+        .paddingInner([0.1])
+        .paddingOuter([0.3]);
+
+    const yScale = d3.scaleLinear()
         .domain([0, yDomainMax])
         .range([height, 0]);
+
     const xAxis = d3.axisBottom()
-        .scale(x1)
+        .scale(xScale)
         .ticks(10)
         .tickSizeOuter(0);
+
     const yAxis = d3.axisRight()
-        .scale(y)
-        .tickValues([Math.ceil(yDomainMax / 2), yDomainMax])
-        .tickFormat(d => `${d}%`)
-        .tickSize(width, 0);
+        .scale(yScale)
+        .ticks(5)
+        .tickSize(width, 0)
+        .tickPadding(10);
 
     const svg = d3.select(chart)
         .attr('width', width + margin.left + margin.right)
-        .attr('height', 0)
+        .attr('height', height + margin.top + margin.bottom)
         .attr('class', 'column-chart');
+
+    svg.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis)
+      .selectAll('text')
+        .style('text-anchor', 'start');
+
+    svg.select('.y')
+      .append('text')
+        .attr('class', 'label')
+        .attr('x', width)
+        .attr('y', -4)
+        .style('text-anchor', 'end');
+
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0, ${height})`)
+        .call(xAxis);
+
+    const bar = svg.selectAll('.bar')
+        .data(data)
+      .enter().append('g')
+        .attr('class', 'bar')
+        .attr('transform', d => `translate(${xScale(d.category) + (xScale.bandwidth() / 4)}, 0)`);
+
+    const rect = bar.append('rect')
+        .attr('x', 1)
+        .attr('y', height)
+        .attr('width', xScale.bandwidth() / 2)
+        .attr('height', 0);
+
+    rect.transition()
+      .delay((d, i) => i * 7.5)
+      .duration(500)
+      .attr('y', d => yScale(d.value || 0))
+      .attr('height', d => height - yScale(d.value || 0));
 
     this.animateFauxDOM(800);
   }
@@ -78,7 +146,6 @@ class ColumnChart extends Component {
         <div className="renderedD3">
           {this.state.chart}
         </div>
-        <img src="images/demo-chart.png" alt="chart" />
       </div>
     );
   }
