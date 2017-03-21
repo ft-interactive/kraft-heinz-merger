d2a89ae0708f32bc3363b9027bb911acc37ebd07
Joanna Kao <joanna.kao@ft.com>
2017-03-20 20:12:10 -0400

    use summary large image for twitter card

--- config/article.js
+++ config/article.js
@@ -54,7 +54,7 @@ export default () => ({ // eslint-disable-line
         Twitter card docs:
         https://dev.twitter.com/cards/markup
   */
-  twitterCard: 'summary',
+  twitterCard: 'summary_large_image',
 
   /*
   TODO: Do you want to tweak any of the
